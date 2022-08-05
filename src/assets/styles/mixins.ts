477641a8e131f9a91c95d4dd78679dc837874f5a
LyoDekken <humberto.araripe12@gmail.com>
2022-08-04 23:42:05 -0300

    feat: starting asset settings

---
+++ src/assets/styles/mixins.ts
@@ -0,0 +1,4 @@
+import { css } from 'styled-components';
+import { colors } from 'assets/styles/colors';
+import { constants } from 'assets/styles/constants';
+import down from 'assets/icons/down.svg';
