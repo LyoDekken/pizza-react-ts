d862e741b271cad7688fb03e4293cb589a8a7d86
LyoDekken <humberto.araripe12@gmail.com>
2022-08-06 00:00:42 -0300

    feat: mapping side menu icons

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,9 +1,40 @@
+import Menu from "components/Menu";
+import { ReactComponent as Search } from "assets/icons/search.svg";
 import * as S from "./styles";
+import { RoutePath } from "types/routes";
+import { navigationItems } from "data/navigation";
 
 export default function Home() {
   return (
-    <div>
-      <S.Home>Hello World</S.Home>
-    </div>
+    <S.Home>
+      <Menu active={RoutePath.HOME} navItems={navigationItems} />
+      <S.HomeContent>
+        <header>
+          <S.HomeHeaderDetails>
+            <div>
+              <S.HomeHeaderDetailsLogo>Pizza Fresh</S.HomeHeaderDetailsLogo>
+              <S.HomeHeaderDetailsDate>
+                Aqui ficará a data
+              </S.HomeHeaderDetailsDate>
+            </div>
+            <S.HomeHeaderDetailsSearch>
+              <Search />
+              <input type="text" placeholder="Procure pelo sabor" />
+            </S.HomeHeaderDetailsSearch>
+          </S.HomeHeaderDetails>
+        </header>
+        <div>
+          <S.HomeProductTitle>
+            <b>Pizzas</b>
+          </S.HomeProductTitle>
+          <S.HomeProductList>
+            <p>Lista de produtos aqui</p>
+          </S.HomeProductList>
+        </div>
+      </S.HomeContent>
+      <aside>
+        <p>Detalhes dos pedidos aqui</p>
+      </aside>
+    </S.Home>
   );
 }
