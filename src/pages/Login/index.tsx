d280d00e623eb9ff766664cb857d5443ec2ec287
LyoDekken <humberto.araripe12@gmail.com>
2022-08-06 16:20:39 -0300

    feat: entering inputs on the login page

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,9 +1,11 @@
 import * as S from "./styles";
+import BoxLogin from "components/BoxLogin/index";
 
 export default function Login() {
   return (
     <S.Login>
       <S.LoginContent>
+				<BoxLogin />
       </S.LoginContent>
     </S.Login>
   );
