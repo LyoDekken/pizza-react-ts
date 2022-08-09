cc9cd545ba3973aa634bf573978d1e5991fbf88c
LyoDekken <humberto.araripe12@gmail.com>
2022-08-08 21:56:37 -0300

    feat: validation of login

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,11 +1,22 @@
 import * as S from "./styles";
-import BoxLogin from "components/BoxLogin/index";
+import BoxLogin from "components/BoxLogin";
+
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { RoutePath } from "types/routes";
 
 export default function Login() {
+  const [errorMessage, setErrorMessage] = useState("");
+  const navigate = useNavigate();
+
+  const handleSubmit = () => {
+    navigate(RoutePath.HOME);
+  };
+
   return (
     <S.Login>
       <S.LoginContent>
-				<BoxLogin />
+        <BoxLogin onSubmitData={handleSubmit} errorMessage={errorMessage} />{" "}
       </S.LoginContent>
     </S.Login>
   );
